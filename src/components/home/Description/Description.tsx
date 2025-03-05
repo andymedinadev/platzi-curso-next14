43f584cfcb52c49ac332625324e6d6b3c7cf4274
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2025-03-05 09:37:53 -0300

    feat: update description component

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,23 @@
+import Image from 'next/image';
+import styles from './Description.module.css';
+
 export const Description = () => {
   return (
-    <section>
-      <h1>Descripción</h1>
+    <section className={styles.section}>
+      <Image
+        className={styles.img}
+        src="/images/description.jpeg"
+        alt="products marketplace"
+        width="500"
+        height="300"
+      />
+      <div>
+        <h2 className={styles.title}>Bring the future today</h2>
+        <p className={styles.description}>
+          Future World: Your Gateway to Tomorrow&#39;s Tech! Dive into a world of cutting-edge
+          gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.
+        </p>
+      </div>
     </section>
   );
 };
