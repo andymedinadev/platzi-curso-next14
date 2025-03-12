fea882c5fa4d6089363e025463a2368ae973f58d
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2025-03-12 12:55:30 -0300

    feat: fetch products from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { Hero } from 'app/components/home/Hero';
 import { Description } from 'app/components/home/Description';
 import { MainProducts } from 'app/components/home/MainProducts';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Description />
-      <MainProducts />
+      <Suspense fallback={<Loading />}>
+        <MainProducts />
+      </Suspense>
     </main>
   );
 }
--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,7 +1,55 @@
-export const MainProducts = () => {
+import Image from 'next/image';
+import styles from './MainProducts.module.css';
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: Rating;
+}
+
+export interface Rating {
+  rate: number;
+  count: number;
+}
+
+const getProducts = async () => {
+  const response = await fetch('https://fakestoreapi.com/products');
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+export const MainProducts = async () => {
+  const products: Product[] = await getProducts();
+
   return (
     <section>
-      <h1>Main Products</h1>
+      <h3 className={styles.title}>✨ New products released!</h3>
+      <div className={styles.container}>
+        {products.map((product) => {
+          const imageSrc = product.image;
+          return (
+            <article key={product.id} className={styles.product}>
+              <p className={styles.product__title}>{product.title}</p>
+              <Image
+                src={imageSrc}
+                className={styles.product__img}
+                fill
+                alt={product.title}
+                loading="eager"
+              />
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
