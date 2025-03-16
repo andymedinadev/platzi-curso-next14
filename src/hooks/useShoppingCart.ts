a2f85b332cc73a0e4b3583cb8696053db2e44f96
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2025-03-16 13:55:48 -0300

    feat: add local storage support for shopping cart and implement item removal

--- src/hooks/useShoppingCart.ts
+++ src/hooks/useShoppingCart.ts
@@ -3,9 +3,41 @@ import { create } from 'zustand';
 type Store = {
   cart: CartItem[];
   addToCart: (cartItem: CartItem) => void;
+  removeCartItem: (cartItem: CartItem) => void;
 };
 
-export const useShoppingCart = create<Store>()((set) => ({
-  cart: [],
-  addToCart: (cartItem: CartItem) => set((state) => ({ cart: [...state.cart, cartItem] })),
+const saveArrayToLocalStorage = (array: CartItem[]) => {
+  localStorage.setItem('cart', JSON.stringify(array));
+};
+
+const loadCartFromLocalStorage = (): CartItem[] => {
+  if (typeof window === 'undefined') return [];
+  const cart = localStorage.getItem('cart');
+  return cart ? JSON.parse(cart) : [];
+};
+
+export const useShoppingCart = create<Store>((set) => ({
+  cart: loadCartFromLocalStorage(),
+
+  addToCart: (cartItem: CartItem) =>
+    set((state) => {
+      const existingItemIndex = state.cart.findIndex((item) => item.id === cartItem.id);
+      const newCart = [...state.cart];
+
+      if (existingItemIndex >= 0) {
+        newCart[existingItemIndex] = cartItem;
+      } else {
+        newCart.push(cartItem);
+      }
+
+      saveArrayToLocalStorage(newCart);
+      return { cart: newCart };
+    }),
+
+  removeCartItem: (cartItem: CartItem) =>
+    set((state) => {
+      const newCart = state.cart.filter((item) => item.id !== cartItem.id);
+      saveArrayToLocalStorage(newCart);
+      return { cart: newCart };
+    }),
 }));
